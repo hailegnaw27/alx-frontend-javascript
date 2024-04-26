c268d020737dd2b8c0bdb4a59445950b6f1f5068
hailegnaw27 <ixvos406@gmail.com>
2024-04-26 15:48:47 -0700

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,11 @@
-export default function* createIteratorObject(report) {
-  // Get the allEmployees property from the report object
-  const allEmployees = report.allEmployees;
-  
-  // Iterate through each department in allEmployees
-  for (const department of Object.values(allEmployees)) {
-    // Iterate through each employee in the current department
-    for (const employee of department) {
-      // Yield the current employee
-      yield employee;
+
+export default function createIteratorObject(report) {
+  return (function* _() {
+    for (const dep of Object.values(report.allEmploys)) {
+      for (const employ of dep) {
+        yield employ;
+      }
     }
-  }
+  })();
 }
+
